02bc62466eda67a23403f72843858019cc2ed1be
Wei Zeng <gzzengwei@gmail.com>
2018-02-28 00:30:05 +1100

    rails 5

--- db/schema.rb
+++ db/schema.rb
@@ -1,4 +1,3 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
@@ -14,31 +13,29 @@
 ActiveRecord::Schema.define(version: 20160617231735) do
 
   create_table "frames", force: :cascade do |t|
-    t.integer  "player_id"
-    t.integer  "round"
-    t.integer  "roll_1"
-    t.integer  "roll_2"
-    t.integer  "roll_3"
+    t.integer "player_id"
+    t.integer "round"
+    t.integer "roll_1"
+    t.integer "roll_2"
+    t.integer "roll_3"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["player_id"], name: "index_frames_on_player_id"
   end
 
-  add_index "frames", ["player_id"], name: "index_frames_on_player_id"
-
   create_table "games", force: :cascade do |t|
-    t.string   "winner"
+    t.string "winner"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
 
   create_table "players", force: :cascade do |t|
-    t.integer  "game_id"
-    t.string   "name"
-    t.integer  "final_score"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.integer "game_id"
+    t.string "name"
+    t.integer "final_score"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["game_id"], name: "index_players_on_game_id"
   end
-
-  add_index "players", ["game_id"], name: "index_players_on_game_id"
 
 end
